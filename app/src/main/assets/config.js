80fbec90d9acb032297af7adc277c16b686e8229
luizclaudiosilvacirilo <luizfz6buiu@gmail.com>
2026-09-09 12:42:31 -0300

    Fix Android app DOM helper

--- app/src/main/assets/config.js
+++ app/src/main/assets/config.js
@@ -1,1 +1,1 @@
-window.KING_DRIVER_SUPABASE_URL='https://zfsvctjqljxchxbnuvui.supabase.co';window.KING_DRIVER_SUPABASE_PUBLISHABLE_KEY='sb_publishable_17rKbweYH7V1E0C0-TPivg_8w4hUx5A';
+window.KING_DRIVER_SUPABASE_URL='https://zfsvctjqljxchxbnuvui.supabase.co';window.KING_DRIVER_SUPABASE_PUBLISHABLE_KEY='sb_publishable_17rKbweYH7V1E0C0-TPivg_8w4hUx5A';window.$=id=>document.getElementById(id);
