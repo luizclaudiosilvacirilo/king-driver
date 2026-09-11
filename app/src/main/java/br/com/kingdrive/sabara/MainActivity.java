e33fe663fd7278fd8e26f030a4ba99a9005684c1
luizclaudiosilvacirilo <luizfz6buiu@gmail.com>
2026-09-10 22:10:45 -0300

    Abrir APK com autenticação por SMS

--- app/src/main/java/br/com/kingdrive/sabara/MainActivity.java
+++ app/src/main/java/br/com/kingdrive/sabara/MainActivity.java
@@ -49,7 +49,7 @@ public class MainActivity extends Activity {
             }
         });
         setContentView(web);
-        web.loadUrl("file:///android_asset/index.html");
+        web.loadUrl("file:///android_asset/index-sms.html");
     }
 
     private boolean hasLocationPermission() {
